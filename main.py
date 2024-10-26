e0818be507966f767f8218e218d6b7dfa27ae9c5
凉巷 <108218569+LSM20001221@users.noreply.github.com>
2024-10-26 20:20:13 +0800

    Update main.py
    更改URL路径

--- main.py
+++ main.py
@@ -159,7 +159,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     data = {
         "touser": to_user,
         "template_id": config["template_id"],
-        "url": "http://weixin.qq.com/download",
+        # "url": "http://weixin.qq.com/download",
+        "url": "www.baidu.com",
         "topcolor": "#FF0000",
         "data": {
             "date": {
